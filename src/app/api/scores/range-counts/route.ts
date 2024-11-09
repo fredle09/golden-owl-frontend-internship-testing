dfe63250822d39a0fb3ee0a93f60b2a333704e75
fredle09 <le197903@gmail.com>
2024-11-09 07:59:35 +0700

    feat: refactor GET request in range-counts API to use fetch for improved readability and error handling

--- src/app/api/scores/range-counts/route.ts
+++ src/app/api/scores/range-counts/route.ts
@@ -1,38 +1,21 @@
 import { badRequestResponse, errorResponse, notFoundResponse, successResponse } from "@/utils/response.handler";
 
+export const config = {
+  runtime: 'edge',
+}
+
 export const GET = async () => {
-  return new Promise<Response>((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    const url = `${process.env.PUBLIC_BACKEND_URL}/v1/scores/range-counts`;
+  try {
+    const res = await fetch(`${process.env.PUBLIC_BACKEND_URL}/v1/scores/range-counts`);
+    const { data, message } = await res.json();
+    if (res.status === 404) return notFoundResponse({ message });
+    if (res.status >= 500) throw new Error(message);
+    if (res.status >= 400) return badRequestResponse({ message });
 
-    xhr.open('GET', url, true); // Open a GET request to the URL
-
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === XMLHttpRequest.DONE) {  // Check if request is complete
-        const status = xhr.status;
-
-        try {
-          const response = JSON.parse(xhr.responseText); // Parse the response text as JSON
-          const { data, message } = response;
-
-          if (status === 404) return resolve(notFoundResponse({ message }));
-          if (status >= 500) throw new Error(message);
-          if (status >= 400) return resolve(badRequestResponse({ message }));
-
-          return resolve(successResponse({ data }));
-        } catch (error) {
-          const message = error instanceof Error ? error.message : 'An unknown error occurred';
-          console.error(">> Error in @GET /api/get-scores/range-counts:", message);
-          return resolve(errorResponse({ message }));
-        }
-      }
-    };
-
-    xhr.onerror = () => {
-      // Handle network errors
-      return reject(new Error('Network error occurred while making the request'));
-    };
-
-    xhr.send();  // Send the request
-  });
-};
+    return successResponse({ data });
+  } catch (error) {
+    const message = error instanceof Error ? error.message : 'An unknown error occurred';
+    console.error(">> Error in @GET /api/get-scores/range-counts:", message);
+    return errorResponse({ message });
+  }
+}
